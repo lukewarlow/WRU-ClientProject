50117160b5b1be30bfa27474ffce620013343d3d
Alexander Atherton <athertona@cardiff.ac.uk>
2017-11-18 19:30:35 +0000

    added a staff css page at the moment does not link with the staff page, added a staff webpage for staff log in, added a database file for storing staff username and passwords, added the models.py files and updated the Server.py files. Added a schema.sql file for the staff username and password.

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, redirect, request, render_template, jsonify
 import random
+import models as dbHandler
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -9,6 +10,19 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def returnFirst():
     if request.method == 'GET':
         return render_template('home.html')
+
+#staff page 
+@app.route('/staff', methods=['POST', 'GET'])
+def home():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        dbHandler.insertUser(username, password)
+        users = dbHandler.retrieveUsers()
+        return render_template('staff.html', users=users)
+    else:
+        return render_template('staff.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,7 @@
+/* for staff log in */
+drop table if exists users;
+    create table users (
+    id integer primary key autoincrement,
+    username text not null,
+    password text not null
+);
