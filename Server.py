83f9e429d0e60eea9438bb34b2657c3d37fc3789
Alexander Atherton <athertona@cardiff.ac.uk>
2017-11-19 12:41:15 +0000

    added an admin page to add staff members to a database, added the route in the server.py but the database needs to be made

--- Server.py
+++ Server.py
@@ -11,7 +11,7 @@ def returnFirst():
     if request.method == 'GET':
         return render_template('home.html')
 
-#staff page 
+#staff page
 @app.route('/staff', methods=['POST', 'GET'])
 def home():
     if request.method=='POST':
@@ -22,6 +22,35 @@ def home():
         return render_template('staff.html', users=users)
     else:
         return render_template('staff.html')
+
+#adding staff to as an admin to a database on the admin page
+@app.route("/admin", methods = ['POST','GET'])
+def staffinfo():
+    if request.method =='GET':
+        return render_template('admin.html')
+    if request.method =='POST':
+        firstName = request.form.get('firstName', default="Error")
+        surname = request.form.get('surname', default="Error")
+        homeLocation = request.form.get('homeLocation', default="Error")
+        dateofbirth = request.form.get('dateofbirth', default="Error")
+        username = request.form.get('username', default="Error")
+        password = request.form.get('password', default="Error")
+
+        print("Added staff member:"+firstName)
+        try:
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            cur.execute("INSERT INTO staffinfo ('firstName', 'surname', 'dateofbirth', 'homeLocation', 'username', 'password')\
+                        VALUES (?,?,?,?,?)",(firstName, surname, dateofbirth, homeLocation, username, password) )
+
+            conn.commit()
+            msg = "Record successfully added"
+        except:
+            conn.rollback()
+            msg = "error in insert operation"
+        finally:
+            conn.close()
+            return msg
 
 
 if __name__ == "__main__":
--- schema.sql
+++ schema.sql
@@ -1,7 +1,19 @@
 /* for staff log in */
+DROP TABLE IF EXISTS staffinfo;
 drop table if exists users;
     create table users (
     id integer primary key autoincrement,
     username text not null,
     password text not null
+);
+
+/* for adding a staff member */
+CREATE TABLE IF NOT EXISTS `staffinfo` (
+  `ID`		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `firstName`	TEXT NOT NULL,
+  `surname`	TEXT NOT NULL,
+  'dateofbirth' numeric,
+  `homeLocation`  TEXT NOT NULL,
+  'username' TEXT NOT NULL,
+  'password' TEXT NOT NULL
 );
