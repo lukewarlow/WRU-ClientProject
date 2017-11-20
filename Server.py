80d458db1ae2c270c522f874a82b53839b6bbb58
Luke Warlow <warlowl@cardiff.ac.uk>
2017-11-20 22:39:26 +0000

    Admin page can now add new staff members.

--- Server.py
+++ Server.py
@@ -1,22 +1,24 @@
 import os
-from flask import Flask, redirect, request, render_template, jsonify
+from flask import Flask, redirect, request, render_template, jsonify, make_response, escape, session
 import random
 import models as dbHandler
+import sqlite3
 app = Flask(__name__)
 
+DATABASE = "database.db"
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/Home", methods=['GET'])
 def returnHome():
     if request.method == 'GET':
-        return render_template('home.html', title = "Homepage", admin = checkIsAdmin())
+        return render_template('home.html', title="Homepage", admin=checkIsAdmin())
 
 @app.route("/EventForm", methods=['POST', 'GET'])
 def returnEventForm():
     # if request.method == 'GET':
     #     return render_template('eventForm.html')
     if request.method == 'GET':
-        return render_template('eventForm.html', title = "Event Form", admin = checkIsAdmin())
+        return render_template('eventForm.html', title="Event Form", admin=checkIsAdmin())
     if request.method == 'POST':
         eventDate = request.form.get('eventDate', default="error")
         postcode = request.form.get('postcode', default="error")
@@ -29,7 +31,7 @@ def returnEventForm():
             conn = sqlite3.connect(DATABASE)
             cur = con.cursor()
             cur.execute("INSERT INTO eventForm ('eventDate', 'postcode', 'eventRegion', 'peopleNum', 'tourNum', 'ageRange', 'comments')\
-                        VALUES (?,?,?,?,?,?,?)",(eventDate, postcode, eventRegion, peopleNum, tourNum, ageRange, comments) )
+                        VALUES (?,?,?,?,?,?,?)", (eventDate, postcode, eventRegion, peopleNum, tourNum, ageRange, comments))
             conn.commit()
             msg = "Record successfully added"
         except:
@@ -38,37 +40,41 @@ def returnEventForm():
         finally:
             conn.close()
 
-#staff page
+# staff page
 @app.route('/Staff', methods=['POST', 'GET'])
 def returnStaff():
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         dbHandler.insertUser(username, password)
         users = dbHandler.retrieveUsers()
-        return render_template('staff.html', users = users, title = "Staff Log In", admin = checkIsAdmin())
+        return render_template('staff.html', users=users, title="Staff Log In", admin=checkIsAdmin())
     else:
-        return render_template('staff.html', title = "Staff Log In", admin = checkIsAdmin())
+        return render_template('staff.html', title="Staff Log In", admin=checkIsAdmin())
 
-#adding staff to as an admin to a database on the admin page
-@app.route("/Admin", methods = ['POST','GET'])
+# adding staff to database on the admin page
+@app.route("/Admin", methods=['POST', 'GET'])
 def returnAdmin():
-    if request.method =='GET':
-        return render_template('admin.html', title = "Admin", admin = True)
-    if request.method =='POST':
+    if request.method == 'GET':
+        return render_template('admin.html', title="Admin", admin=True)
+    if request.method == 'POST':
         firstName = request.form.get('firstName', default="Error")
         surname = request.form.get('surname', default="Error")
-        homeLocation = request.form.get('homeLocation', default="Error")
-        dateofbirth = request.form.get('dateofbirth', default="Error")
-        username = request.form.get('username', default="Error")
+        username = surname + firstName[0]
+        username = username.lower()
+        response = checkIfUserExists(username)
+        if (response.split(":")[0] == "True"):
+            username = username + response.split(":")[1]
         password = request.form.get('password', default="Error")
+        usertype = request.form.get('usertype', default="Error")
 
-        print("Added staff member:"+firstName)
+        print("Added staff member:" + firstName)
+
         try:
             conn = sqlite3.connect(DATABASE)
             cur = conn.cursor()
-            cur.execute("INSERT INTO staffinfo ('firstName', 'surname', 'dateofbirth', 'homeLocation', 'username', 'password')\
-                        VALUES (?,?,?,?,?)",(firstName, surname, dateofbirth, homeLocation, username, password) )
+            cur.execute("INSERT INTO tblStaff ('username', 'password', 'usertype', 'firstname', 'surname')\
+                        VALUES (?,?,?,?,?)", (username, password, usertype, firstName, surname))
 
             conn.commit()
             msg = "Record successfully added"
@@ -81,6 +87,23 @@ def returnAdmin():
 
 def checkIsAdmin():
     return True
+
+def checkIfUserExists(username):
+    try:
+        conn = sqlite3.connect(DATABASE)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM tblStaff WHERE username=?;", [username])
+        data = cur.fetchall()
+    except:
+        print('there was an error', data)
+        data = ""
+    finally:
+        conn.close()
+        if (len(data) > 0):
+            return "True:{}".format(len(data) + 1)
+        else:
+            return "False"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/temp/adminpage.js
+++ static/js/temp/adminpage.js
@@ -1,19 +1,23 @@
-function loadName() {
+function addStaff()
+{
   var firstName = document.forms["addstaff"]["firstName"].value;
   var surname = document.forms["addstaff"]["surname"].value;
-  var dateofbirth = document.forms["addstaff"]["dateofbirth"].value;
-  var homeLocation = document.forms["addstaff"]["homeLocation"].value;
-  var username = document.forms["addstaff"]["username"].value;
   var password = document.forms["addstaff"]["password"].value;
-  params = 'firstName='+firstName+'&surname='+surname+'&dateofbirth='+dateofbirth+'&homeLocation='+homeLocation+'&username='+username+'&password='+password;
+  var usertype = document.forms["addstaff"]["usertype"].value;
+  document.forms["addstaff"].reset();
+  params = 'firstName='+firstName+'&surname='+surname+'&password='+password+'&usertype='+usertype;
   var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", '/admin', true); // true is asynchronous
+  xhttp.open("POST", '/Admin', true); // true is asynchronous
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.onload = function() {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
+  xhttp.onload = function()
+  {
+    if (xhttp.readyState === 4 && xhttp.status === 200)
+    {
       console.log(xhttp.responseText);
       document.getElementById("txt").innerHTML = xhttp.responseText;
-    } else {
+    }
+    else
+    {
       console.error(xhttp.statusText);
     }
   };
